bcd92891389624ba6a435c979576ca23c7b51381
OsamaE96 <osama.e96@outlook.com>
2021-08-20 13:55:34 +0300

    Add To Cart Ajax Notification

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,26 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function tempAlert(msg, duration) {
-    var el = document.createElement("div");
-    el.setAttribute("style", "position:absolute;top:40%;left:20%;background-color:white;");
-    el.innerHTML = msg;
-    setTimeout(function () {
-        el.parentNode.removeChild(el);
-    }, duration);
-    document.body.appendChild(el);
-}
 
 function AddToCart(id){
     jQuery.ajax({
         type: 'POST',
         url: '/Cart/AddToCart/' + id,
         dataType: 'json',
-        success: function (response) {
-            alert(response);
+        success: function () {
+            $("#Success").show().delay(2000).hide("slow");
         },
         error: function () {
-            alert("Unable to perform this action");
+            $("#Fail").show().delay(2000).hide("slow");
         },
 
     });
